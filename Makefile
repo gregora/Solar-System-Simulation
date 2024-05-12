5a8671bff9b70f86c6b48d5ae81944db0ae33ded
Gregor A <gregor372@gmail.com>
2024-05-12 11:22:32 +0200

    Move Planet class to src/Planet.cpp

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
-simulation: simulation.cpp src/ui.cpp
-	g++ -std=c++11 -o simulation simulation.cpp src/ui.cpp -ljsoncpp -lsfml-graphics -lsfml-window -lsfml-system
+all: simulation;
+
+Planet.o: src/Planet.cpp src/ui.cpp include/Planet.h include/ui.h
+	g++ -std=c++11 -c src/Planet.cpp -lsfml-graphics -lsfml-window -lsfml-system
+
+simulation: simulation.cpp src/ui.cpp Planet.o include/Planet.h include/ui.h
+	g++ -std=c++11 -o simulation Planet.o simulation.cpp src/ui.cpp -ljsoncpp -lsfml-graphics -lsfml-window -lsfml-system
---
+++ include/Planet.h
@@ -0,0 +1,37 @@
+#ifndef _PLANET_H
+#define _PLANET_H
+
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+class Planet: public sf::Transformable, public sf::Drawable
+{
+    public:
+    std::string name;
+    bool is_moon = false;
+    double mass;
+    double diameter;
+    double x, y, z;
+    double vx, vy, vz;
+    double ax, ay, az;
+
+    sf::Color color;
+
+    float max_history = 100;
+    std::vector<double> x_history;
+    std::vector<double> y_history;
+    bool draw_history = true;
+
+    Planet(std::string name, double mass, double diameter, double x, double y, double z, double vx, double vy, double vz, sf::Color color = sf::Color(0, 0, 255), bool draw_history = true, bool is_moon = false);
+
+    Planet();
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+
+};
+
+void simulate(Planet planets[], uint N, float delta, bool save_history = false);
+
+
+#endif
